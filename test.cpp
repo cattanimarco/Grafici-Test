e11931ce4a0fd2092df1df7ec92cfca82d6ba4f4
m.cattani@gmail.com <m.cattani@gmail.com>
2020-07-01 14:15:32 +0100

    Aligned test and examples part 2

--- test.cpp
+++ test.cpp
@@ -205,22 +205,24 @@ int main()
 
 	{
 		/* Polar Boundary */
+		File_GFX gfx(640, 480, "imgs/polar_boundary.bmp");
 
 		constexpr size_t source_data_size = 9;
 		constexpr size_t spline_size = 49;
 		float array[source_data_size] = { 0, 2, 0, 2, 0, 1, 0, 1, 0 };
-
-		File_GFX gfx(640, 480, "imgs/polar_boundary.bmp");
-		grafici.begin(gfx, Colors::parula);
-		grafici.clear();
-
-		/* Load raw data */
 		Linear x(source_data_size);
 		ArrayFloat y(array, source_data_size);
 		Constant opt(source_data_size, 0.0);
 		SplineInterpolator dataSpline{ x, y, y, opt, spline_size };
-
 		PolarBoundary leftBoundary;
+		PolarBoundary rightBoundary;
+		PolarBoundary rightBottomBoundary;
+		PolarBoundary middleBottomBoundary;
+		PolarBoundary rightTopBoundary;
+
+		grafici.begin(gfx, Colors::parula);
+		grafici.clear();
+
 		leftBoundary.cropGridCartesian(1, 2, 0, 0);
 		leftBoundary.cropAbsoluteCartesian({ 0.02, 0.02 }, { 0.02, 0.02 });
 		leftBoundary.cropGridPolar(1, 2, 0, 0);
@@ -229,7 +231,6 @@ int main()
 		grafici.colorMap(Colors::parula);
 		grafici.plot(bar, dataSpline, leftBoundary);
 
-		PolarBoundary rightBoundary;
 		rightBoundary.cropGridCartesian(1, 2, 0, 1);
 		rightBoundary.cropAbsoluteCartesian({ 0.02, 0.02 }, { 0.02, 0.02 });
 		rightBoundary.cropRelativePolar({ 0, 0.25 }, { 0, 0.0 });
@@ -237,19 +238,16 @@ int main()
 		grafici.plot(axis, Linear(28), Linear(5), Constant(10, 0), rightBoundary);
 		grafici.colorMap(Colors::parula);
 
-		PolarBoundary rightBottomBoundary;
 		rightBottomBoundary.cropAbsoluteCartesian({ 0.02, 0.02 }, { 0.02, 0.02 });
 		rightBottomBoundary.cropGridCartesian(1, 2, 0, 1);
 		rightBottomBoundary.cropRelativePolar({ 0, 0.25 }, { 0, 0.5 });
 		grafici.plot(bar, dataSpline, rightBottomBoundary);
 
-		PolarBoundary middleBottomBoundary;
 		middleBottomBoundary.cropAbsoluteCartesian({ 0.02, 0.02 }, { 0.02, 0.02 });
 		middleBottomBoundary.cropGridCartesian(1, 2, 0, 1);
 		middleBottomBoundary.cropRelativePolar({ 0, 0.25 }, { 0.35, 0.35 });
 		grafici.plot(scatter, dataSpline.x(), dataSpline.y(), dataSpline.c(), Constant(spline_size, 0.01), middleBottomBoundary);
 
-		PolarBoundary rightTopBoundary;
 		rightTopBoundary.cropAbsoluteCartesian({ 0.02, 0.02 }, { 0.02, 0.02 });
 		rightTopBoundary.cropGridCartesian(1, 2, 0, 1);
 		rightTopBoundary.cropRelativePolar({ 0, 0.25 }, { 0.5, 0.0 });
@@ -260,16 +258,9 @@ int main()
 
 	{
 		/* Colors */
-		constexpr size_t data_size = 240;
-
 		File_GFX gfx(640, 480, "imgs/colors.bmp");
-		grafici.begin(gfx, Colors::parula);
-		grafici.clear();
 
-		Linear x(data_size);
-		Constant y(data_size, 1.0);
-		Constant opt(data_size, 1.0);
-
+		constexpr size_t data_size = 240;
 		const ColorMap *colorPlots[] = { &Colors::rainbow,
 			                             &Colors::temperature,
 			                             &Colors::blackAndWhite,
@@ -278,6 +269,12 @@ int main()
 			                             &Colors::bright,
 			                             &Colors::semaphore,
 			                             &Colors::parula };
+		Linear x(data_size);
+		Constant y(data_size, 1.0);
+		Constant opt(data_size, 1.0);
+
+		grafici.begin(gfx, Colors::parula);
+		grafici.clear();
 
 		for (size_t idx = 0; idx < sizeof(colorPlots) / sizeof(ColorMap *); idx++)
 		{
@@ -286,27 +283,28 @@ int main()
 			boundary.cropGridCartesian(8, 1, idx).cropAbsoluteCartesian({ 0.01, 0.01 }, { 0.01, 0.01 });
 			grafici.plot(bar, x, y, x, opt, boundary);
 		}
+
 		gfx.flush();
 	}
 
 	{
 		/* shared axis */
+		File_GFX gfx(640, 480, "imgs/shared_axis.bmp");
+
 		constexpr size_t num_elem = 5;
 		float array_y1[num_elem] = { 1, 2, 3, 4, 5 };
 		float array_y2[num_elem] = { 2, 3, 4, 5, 6 };
 		float array_x1[num_elem] = { 2, 3, 4, 5, 6 };
 		float array_x2[num_elem] = { 0, 1, 2, 3, 4 };
-
-		File_GFX gfx(640, 480, "imgs/shared_axis.bmp");
-		grafici.begin(gfx, Colors::blackAndWhite);
-		grafici.clear();
-
 		Linear xAxis(7);
 		Linear yAxis(6, -1, 1);
 		ArrayFloat y1(array_y1, num_elem);
 		ArrayFloat y2(array_y2, num_elem);
 		ArrayFloat x1(array_x1, num_elem);
 		ArrayFloat x2(array_x2, num_elem);
+
+		grafici.begin(gfx);
+		grafici.clear();
 
 		/* set each limit as the union (using + sign) of all the limits*/
 		x1.limits() = x2.limits() = xAxis.limits() = (x1.limits() + x2.limits() + xAxis.limits());
@@ -315,49 +313,11 @@ int main()
 		grafici.colorMap(Colors::blackAndWhite);
 		grafici.plot(axis, xAxis, yAxis, Constant(8, 0.1));
 		grafici.colorMap(Colors::semaphore);
-
 		grafici.plot(line, x1, y1, y1);
 		grafici.plot(line, x2, y2, y2);
+
 		gfx.flush();
 	}
-
-	// {
-	// 	/* Colors */
-	// 	DataSources::Float dataset;
-	// 	DataSources::LinearInterpolator dataLinearInterpolator;
-	// 	RoundBoundary roundBoundary;
-
-	// 	float dataArray[2] = { 2, 2 };
-
-	// 	File_GFX gfx(640, 480, "imgs/colors.bmp");
-
-	// 	grafici.begin(gfx);
-	// 	dataset.begin(dataArray, 1, 2);
-	// 	dataset.yMin = 0; /* ovverride min value */
-	// 	dataLinearInterpolator.begin(dataset, 100);
-	// 	grafici.style.colorSource = ColorSource::computeFromX;
-
-	// 	grafici.clear();
-
-	// 	ColorSet colorPlots[] = { csBright,
-	// 		                      csBw,
-	// 		                      csCmyk,
-	// 		                      csFrance,
-	// 		                      heat,
-	// 		                      semaphore,
-	// 		                      parula,
-	// 		                      csRainbow };
-
-	// 	for (int i = 0; i < sizeof(colorPlots) / sizeof(ColorSet); i++)
-	// 	{
-	// 		grafici.style.setPalette(colorPlots[i]);
-	// 		roundBoundary.fullScreen().subBoundary(2, 4, i).crop(0.02, 0.02, 0.02, 0.02);
-	// 		roundBoundary.cropRadial(0, 0.25, 0, 0);
-	// 		grafici.plot(barPlot, dataLinearInterpolator, roundBoundary);
-	// 	}
-
-	// 	gfx.flush();
-	// }
 
 	return 0;
 }
